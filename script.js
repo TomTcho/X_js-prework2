20d29ee65813b2925feac921099d8c441853f49c
Tomasz Tchorzewski <tomasz.tchorzewski@me.com>
2022-05-04 19:46:05 +0200

    zakończony prework2

---
+++ script.js
@@ -0,0 +1,30 @@
+/* PODSTAWOWE TYPY DANYCH! */
+
+let name = 'Monika';
+
+console.log(name);
+
+
+let number = 108;
+
+console.log(2 * number);
+
+let boolean = true;
+
+
+console.log(boolean);
+
+
+let array = [1, 2, 3, 108];
+
+console.log(array);
+
+
+let objectH1 = document.querySelector('h1');
+
+console.log(objectH1);
+
+objectH1.innerText = 'Kurs Javascript';
+
+console.log(objectH1);
+
